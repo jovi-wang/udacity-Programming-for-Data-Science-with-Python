2b8aba872ebcd15b64866a2e4587b33202897278
jovi <jovi.wang@team.telstra.com>
2023-03-26 14:11:38 +1100

    add print statment after user type no

--- bikeshare.py
+++ bikeshare.py
@@ -221,6 +221,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
+    print('Thanks for using our program!')
 
 if __name__ == "__main__":
 	main()
